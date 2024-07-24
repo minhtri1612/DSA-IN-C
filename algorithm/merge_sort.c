a0557110e041c475be13e0250e082bb3a4f4f865
minhtri1612 <10422119@student.vgu.edu.vn>
2024-07-25 00:40:44 +0700

    new

--- algorithm/merge_sort.c
+++ algorithm/merge_sort.c
@@ -4,9 +4,7 @@ void merge(int arr[], int left, int mid, int right) {
     int i, j, k;
     int n1 = mid - left + 1;
     int n2 = right - mid;
-
     int L[n1], R[n2];
-
     for (i = 0; i < n1; i++)
         L[i] = arr[left + i];
     for (j = 0; j < n2; j++)
